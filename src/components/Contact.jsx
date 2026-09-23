d9e24bf97409c42fcf5119105f420567b23d3ab4
Vishesh Gaur <visheshgaur276@gmail.com>
2026-09-23 21:17:54 +0530

    fix linkedin link

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -33,7 +33,7 @@ function Contact() {
           </a>
 
           <a
-            href="https://www.linkedin.com/"
+            href="https://www.linkedin.com/in/vishesh-gaur-ba2935226/"
             target="_blank"
             rel="noopener noreferrer"
           >
